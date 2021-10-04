1e426f0274b0a35dc4b2b70562d0f754d0b9ca35
Fadi Sarwat <fadisarwat@yahoo.com>
2021-10-04 16:01:10 +0200

    Add agent and ip to fillables

--- src/RememberToken.php
+++ src/RememberToken.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 class RememberToken extends Model
 {
     protected $fillable = [
-        'token', 'user_id', 'expires_at',
+        'token', 'user_id', 'expires_at', 'agent', 'ip'
     ];
 
     public function user()
